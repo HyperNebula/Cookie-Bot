c333aea7f2e8e5c7e5ee5300a986d567a8a64699
TheNexus <36902158+The-Canadians-Friend@users.noreply.github.com>
2018-09-14 22:16:07 -0400

    Added pay command
    You can pay others your money!

--- account.py
+++ account.py
@@ -45,7 +45,7 @@ def bal(name):
             namenum += 1
             if line[0] == name:
                 balance = line[1]
-                return "Your balance is {}".format(balance)
+                return balance
             elif namenum == count():
                 return "Please register first using `/bank register`"
 
@@ -72,7 +72,6 @@ def top():
     df.to_csv('accounts.csv', index=False)
 
     with open('accounts.csv', 'r') as readin:
-        topReader = csv.reader(readin)
         counter = 0
         leaderboard = []
 
@@ -88,3 +87,31 @@ def top():
 
         return leaderboard
 
+
+def pay(startuser, enduser, amount):
+    if amount < 1:
+        return 'The minimum amount of Cocoa Beans is 1'
+    balnum = bal(startuser)
+    if balnum == "Please register first using `/bank register`":
+        return "Please register first using `/bank register`"
+    if amount > int(balnum):
+        return 'You do not have enough Cocoa Beans. Use /bank to see your current balance'
+
+    with open('accounts.csv', 'r') as userl:
+        userReader = csv.reader(userl)
+        namenum = -1
+
+        for line in userReader:
+            namenum += 1
+            if line[0] == enduser:
+                df = pd.read_csv('accounts.csv')
+                df.loc[df["UserId"] == int(enduser), "Balance"] += amount
+                df.to_csv('accounts.csv', index=False)
+
+                df = pd.read_csv('accounts.csv')
+                df.loc[df["UserId"] == int(startuser), "Balance"] -= amount
+                df.to_csv('accounts.csv', index=False)
+
+                return 'Payment successful!'
+            elif namenum == count():
+                return "That user does not exist or has not registered a bank account."
